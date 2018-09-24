dabd95d9dfa48375601a7a18433d641249652139
cc <15355498770@163.com>
2018-09-24 16:12:06 +0800

    add addTwoNumbers by go

---
+++ 2018/september/0924/addTwoNumbers.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	return nil
+}
+
+func getNext(node ListNode) (nextNode ListNode, hasNext bool) {
+
+
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
