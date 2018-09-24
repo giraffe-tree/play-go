60aa4bd8f763c31de8a610da02f184dbb39edb8e
giraffetree <15355498770@163.com>
2018-09-24 23:15:14 +0800

    add go learning - 09/24

--- 2018/september/0924/addTwoNumbers.go
+++ 2018/september/0924/addTwoNumbers.go
@@ -17,6 +17,26 @@ https://leetcode-cn.com/problems/add-two-numbers/description/
 
 */
 func main() {
+
+	// 初始化 struct 的两种方式
+	// b和c的初始化形式相同
+
+	a := ListNode{Val: 1}
+	b := &ListNode{Val: 1}
+	c := new(ListNode)
+
+	// cannot use _ as value
+	// var _ = ListNode{0, nil}
+
+	fmt.Println(a.Val)
+	fmt.Println(b.Val)
+	fmt.Println(c.Val)
+	fmt.Println(a)
+	fmt.Println(b)
+
+}
+
+func test() {
 	node1 := new(ListNode)
 	node12 := new(ListNode)
 	node13 := new(ListNode)
@@ -101,4 +121,32 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+func addTwoNumbers2(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	// copy from leetcode
+	dummy := &ListNode{}
+	current, carry := dummy, 0
+
+	for l1 != nil || l2 != nil {
+		val := carry
+		if l1 != nil {
+			val += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			val += l2.Val
+			l2 = l2.Next
+		}
+		carry, val = val/10, val%10
+		current.Next = &ListNode{Val: val}
+		current = current.Next
+	}
+
+	if carry != 0 {
+		current.Next = &ListNode{Val: carry}
+	}
+
+	return dummy.Next
 }
---
+++ 2018/september/0924/referenceTest.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+/**
+用于测试 golang 中的引用问题
+由于在 java 中都是值传递, 想看看在 go 中是怎么样的
+*/
+func main() {
+	apple := &Apple{"cc", 1}
+	fmt.Printf("原始 apple 地址:%p", &apple)
+	fmt.Println(apple.name, apple.size) // cc 1
+
+	fmt.Printf("原始 apple 地址:%p\n", &apple)
+	test1(*apple)
+	fmt.Println(apple.name, apple.size) // cc 1
+
+	fmt.Printf("原始 apple 地址:%p\n", &apple)
+	test2(apple)
+	fmt.Println(apple.name, apple.size) // chen 10
+
+	fmt.Printf("原始 apple 地址:%p\n", &apple)
+	apple = &Apple{"cc", 1}
+	test3(apple)
+	fmt.Println(apple.name, apple.size) // chen 333
+
+	fmt.Printf("原始 apple 地址:%p\n", &apple)
+	apple = &Apple{"cc", 1}
+	test4(apple)
+	fmt.Println(apple.name, apple.size) // chen 10
+	ints := make(map[string]int)
+	fmt.Println(ints)
+}
+
+// 引用了变量 apple 的一个副本, 及时在函数内部改变对象, 也不能影响原对象, 故原对象状态不改变
+func test1(apple Apple) {
+	fmt.Printf("test1 apple 地址:%p", &apple)
+	apple.name = "chen"
+	apple.size = 10
+}
+
+// 将变量 apple 的指针传递给函数, 隐式地将 apple 的地址也传递给了函数
+// 所以函数执行后, 原变量的状态发生了改变
+func test2(apple *Apple) {
+	fmt.Printf("test2 apple 地址:%p", &apple)
+	apple.name = "chen"
+	apple.size = 10
+}
+
+// 对变量进行重新赋值
+func test3(apple *Apple) {
+	fmt.Printf("test3 apple 地址:%p\n", &apple)
+	*apple = Apple{"chen", 333}
+	fmt.Printf(" apple 地址:%p\n", &apple)
+}
+
+func test4(apple *Apple) {
+	fmt.Printf("test4 apple 地址:%p\n", &apple)
+	apple = &Apple{"chen", 444}
+	fmt.Printf(" apple 地址:%p\n", &apple)
+}
+
+type Apple struct {
+	name string
+	size int
+}
