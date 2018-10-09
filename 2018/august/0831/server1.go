e3a9ab1d38c65f30661e685542a3379bbcd3e616
giraffetree <15355498770@163.com>
2018-10-09 18:05:48 +0800

    add com at 10/09

--- 2018/august/0831/server1.go
+++ 2018/august/0831/server1.go
@@ -9,10 +9,14 @@ import (
 func main() {
 
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe("192.168.2.127:8888", nil))
 
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.PATH= %q\n ", r.URL.Path)
+	var url = " http://www.protontek.com/alipay/reportDetail.html?object_key=release/ios/2018/08/09/3787/15337941186.json&apiuid=3787&apitoken=3C82B887BACA4FE7E361EE2E75DA4074"
+
+	var text = "<html>\n<body>\n<h1>我的第一个标题</h1>\n<a href=\"" + url + "\">Link text</a>\n</body>\n</html>"
+	fmt.Fprintf(w, text)
+
 }
